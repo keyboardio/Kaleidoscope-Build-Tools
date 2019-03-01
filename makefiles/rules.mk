758337d56ecd69c627f6b62b7781658111367f6d
Jesse Vincent <jesse@keyboard.io>
2019-02-28 22:37:23 -0800

    build-tools is not inside 'avr' anymore

--- makefiles/rules.mk
+++ makefiles/rules.mk
@@ -1,5 +1,5 @@
 EXTRA_BUILDER_ARGS="-libraries ."
-PLUGIN_TEST_SUPPORT_DIR ?= $(BOARD_HARDWARE_PATH)/keyboardio/avr/build-tools/
+PLUGIN_TEST_SUPPORT_DIR ?= $(BOARD_HARDWARE_PATH)/keyboardio/build-tools/
 PLUGIN_TEST_BIN_DIR ?= $(PLUGIN_TEST_SUPPORT_DIR)/../toolchain/$(shell gcc --print-multiarch)/bin
 
 KALEIDOSCOPE_BUILDER_DIR ?= $(BOARD_HARDWARE_PATH)/keyboardio/avr/libraries/Kaleidoscope/bin/
