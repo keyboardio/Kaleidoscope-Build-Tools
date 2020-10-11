49566242a384dc55d6686d768da9035288b5a850
Jesse Vincent <jesse@keyboard.io>
2020-10-10 19:25:42 -0700

    remove some obsolete makefile targets
    (all of them travis-specific)

--- makefiles/rules.mk
+++ makefiles/rules.mk
@@ -5,18 +5,11 @@ PLUGIN_TEST_BIN_DIR ?= $(PLUGIN_TEST_SUPPORT_DIR)/../toolchain/$(shell gcc --pri
 KALEIDOSCOPE_BUILDER_DIR ?= $(BOARD_HARDWARE_PATH)/keyboardio/avr/libraries/Kaleidoscope/bin/
 
 
-TRAVIS_DOWNLOAD_DIR ?= $(shell pwd)/.download-cache
-TRAVIS_ARDUINO ?= arduino-1.8.13
-TRAVIS_ARDUINO_FILE ?= $(TRAVIS_ARDUINO)-linux64.tar.xz
-TRAVIS_ARDUINO_PATH ?= $(shell pwd)/$(TRAVIS_ARDUINO)
-TRAVIS_ARDUINO_DOWNLOAD_URL = http://downloads.arduino.cc/$(TRAVIS_ARDUINO_FILE)
-
 SHELL_FILES = $(shell if [ -d bin ]; then egrep -n -r -l "(env (ba)?sh)|(/bin/(ba)?sh)" bin; fi)
 
 
-# TODO check the shasum of the travis arduino file
 
-.PHONY: travis-install-arduino astyle travis-test travis-check-astyle travis-smoke-examples test cpplint cpplint-noisy shellcheck
+.PHONY: astyle test cpplint cpplint-noisy shellcheck smoke-examples
 
 .DEFAULT_GOAL := all
 
@@ -25,8 +18,6 @@ all: build-all
 
 astyle:
 	PATH="$(PLUGIN_TEST_BIN_DIR):$(PATH)" $(PLUGIN_TEST_SUPPORT_DIR)/quality/run-astyle
-
-travis-test: find-filename-conflicts travis-smoke-examples travis-check-astyle shellcheck
 
 test: smoke-examples check-astyle cpplint check-docs
 
@@ -46,7 +37,7 @@ check-astyle: astyle
 	PATH="$(PLUGIN_TEST_BIN_DIR):$(PATH)" $(PLUGIN_TEST_SUPPORT_DIR)/quality/astyle-check
 
 cpplint-noisy:
-	-$(PLUGIN_TEST_SUPPORT_DIR)/quality/cpplint.py  --filter=-legal/copyright,-build/include,-readability/namespace,-whitespace/line_length,-runtime/references  --recursive --extensions=cpp,h,ino --exclude=$(BOARD_HARDWARE_PATH) --exclude=$(TRAVIS_ARDUINO) src examples
+	-$(PLUGIN_TEST_SUPPORT_DIR)/quality/cpplint.py  --filter=-legal/copyright,-build/include,-readability/namespace,-whitespace/line_length,-runtime/references  --recursive --extensions=cpp,h,ino --exclude=$(BOARD_HARDWARE_PATH) src examples
 
 
 cpplint:
@@ -61,35 +52,6 @@ find-filename-conflicts:
 	@if [ -d "bin" ]; then \
 		bin/find-filename-conflicts; \
 	fi
-
-## NOTE: HERE BE DRAGONS, DO NOT CLEAN THIS UP!
-# When building outside of Arduino-Boards, we want to use the current library,
-# not whatever version is in Arduino-Boards at the time. To do so, we must tell
-# arduino-builder about the library, because the current directory is not
-# considered by default.
-#
-# Now, we can't use -libraries ., because arduino-builder will search for a
-# directory named like the library, and we don't have that. We are already in
-# the library directory at this point.
-#
-# So, we need a library outside of the current dir. We could use .., but we want
-# to be safe, to only have the current library there. For this reason, the
-# travis-smoke-examples target will create a current-libraries directory one
-# level up, symlink the current directory there, under the library name, and add
-# -libraries $(pwd)/../current-libraries to the arduino-builder arguments.
-#
-# All of this makes the builder consider the current library the best to use.
-travis-smoke-examples: travis-install-arduino
-	install -d ../current-libraries
-	ln -s $$(pwd) ../current-libraries/
-	ARDUINO_PATH="$(TRAVIS_ARDUINO_PATH)" BOARD_HARDWARE_PATH="$(BOARD_HARDWARE_PATH)" EXTRA_BUILDER_ARGS="-libraries $$(pwd)/../current-libraries" $(KALEIDOSCOPE_BUILDER_DIR)/kaleidoscope-builder compile-all
-	rm -rf ../current-libraries
-
-travis-simulator-tests: travis-install-arduino simulator-tests
-	@: ## Do not remove this line, otherwise this will trigger the `%` rule too.
-
-travis-check-astyle: check-astyle
-
 
 SMOKE_SKETCHES=$(shell if [ -d ./examples ]; then find ./examples -type f -name \*ino | xargs -n 1 dirname; fi)
 
@@ -109,14 +71,3 @@ $(SMOKE_SKETCHES): force
 %:
 	BOARD_HARDWARE_PATH="$(BOARD_HARDWARE_PATH)" $(KALEIDOSCOPE_BUILDER_DIR)/kaleidoscope-builder $@
 
-
-travis-install-arduino:
-	@if [ ! -f "$(TRAVIS_DOWNLOAD_DIR)/$(TRAVIS_ARDUINO_FILE)" ]; then \
-		echo "Downloading Arduino..."; \
-		mkdir -p "$(TRAVIS_DOWNLOAD_DIR)"; \
-		wget -O "$(TRAVIS_DOWNLOAD_DIR)/$(TRAVIS_ARDUINO_FILE)" -c $(TRAVIS_ARDUINO_DOWNLOAD_URL); \
-	fi
-	@if [ ! -d "$(TRAVIS_ARDUINO_PATH)" ]; then \
-		echo "Installing Arduino..."; \
-		tar xf "$(TRAVIS_DOWNLOAD_DIR)/$(TRAVIS_ARDUINO_FILE)"; \
-	fi
