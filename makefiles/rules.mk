730790f5c09f819189510c0c853caef1ec5406a6
Jesse Vincent <jesse@keyboard.io>
2020-10-15 19:35:40 -0700

    Remove a bunch of makefile targets that are really only for Kaleidoscope the project

--- makefiles/rules.mk
+++ makefiles/rules.mk
@@ -5,40 +5,14 @@ PLUGIN_TEST_BIN_DIR ?= $(PLUGIN_TEST_SUPPORT_DIR)/../toolchain/$(shell gcc --pri
 KALEIDOSCOPE_BUILDER_DIR ?= $(BOARD_HARDWARE_PATH)/keyboardio/avr/libraries/Kaleidoscope/bin/
 
 
-SHELL_FILES = $(shell if [ -d bin ]; then egrep -n -r -l "(env (ba)?sh)|(/bin/(ba)?sh)" bin; fi)
-
-
-
-.PHONY: astyle test cpplint cpplint-noisy shellcheck smoke-examples
-
 .DEFAULT_GOAL := all
 
 all: build-all
 	@: ## Do not remove this line, otherwise `make all` will trigger the `%` rule too.
 
-astyle:
-	PATH="$(PLUGIN_TEST_BIN_DIR):$(PATH)" $(PLUGIN_TEST_SUPPORT_DIR)/quality/run-astyle
-
-test: check-astyle cpplint check-docs
-
 check-docs:
 	doxygen $(PLUGIN_TEST_SUPPORT_DIR)/quality/etc/check-docs.conf 2> /dev/null >/dev/null
 	python $(PLUGIN_TEST_SUPPORT_DIR)/quality/doxy-coverage.py /tmp/undocced/xml
-
-check-astyle: astyle
-	PATH="$(PLUGIN_TEST_BIN_DIR):$(PATH)" $(PLUGIN_TEST_SUPPORT_DIR)/quality/astyle-check
-
-cpplint-noisy:
-	-$(PLUGIN_TEST_SUPPORT_DIR)/quality/cpplint.py  --filter=-legal/copyright,-build/include,-readability/namespace,-whitespace/line_length,-runtime/references  --recursive --extensions=cpp,h,ino --exclude=$(BOARD_HARDWARE_PATH) src examples
-
-
-cpplint:
-	$(PLUGIN_TEST_SUPPORT_DIR)/quality/cpplint.py  --quiet --filter=-whitespace,-legal/copyright,-build/include,-readability/namespace,-runtime/references  --recursive --extensions=cpp,h,ino src examples
-
-shellcheck:
-	@if [ -d "bin" ]; then \
-		shellcheck ${SHELL_FILES}; \
-	fi
 
 %:
 	BOARD_HARDWARE_PATH="$(BOARD_HARDWARE_PATH)" $(KALEIDOSCOPE_BUILDER_DIR)/kaleidoscope-builder $@
