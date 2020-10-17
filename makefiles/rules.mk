7b49bf27ebe399b886e6b274c5a22180a9071dc5
Jesse Vincent <jesse@keyboard.io>
2020-10-16 20:52:11 -0700

    remove check-docs target, which belongs on kaleidoscope

--- makefiles/rules.mk
+++ makefiles/rules.mk
@@ -10,10 +10,6 @@ KALEIDOSCOPE_BUILDER_DIR ?= $(BOARD_HARDWARE_PATH)/keyboardio/avr/libraries/Kale
 all: build-all
 	@: ## Do not remove this line, otherwise `make all` will trigger the `%` rule too.
 
-check-docs:
-	doxygen $(PLUGIN_TEST_SUPPORT_DIR)/quality/etc/check-docs.conf 2> /dev/null >/dev/null
-	python $(PLUGIN_TEST_SUPPORT_DIR)/quality/doxy-coverage.py /tmp/undocced/xml
-
 %:
 	BOARD_HARDWARE_PATH="$(BOARD_HARDWARE_PATH)" $(KALEIDOSCOPE_BUILDER_DIR)/kaleidoscope-builder $@
 
