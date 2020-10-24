f979cd8146af5ebeb316c47a2a6df5db28f391f3
Jesse Vincent <jesse@keyboard.io>
2020-10-23 18:42:39 -0700

    The new arduino-cli based build system will use a new makefile

---
+++ makefiles/sketch-arduino-cli.mk
@@ -0,0 +1,11 @@
+KALEIDOSCOPE_BUILDER_DIR ?= $(BOARD_HARDWARE_PATH)/keyboardio/avr/libraries/Kaleidoscope/bin/
+
+.DEFAULT_GOAL := compile
+
+all: 
+	@echo "Make all target doesn't do anything"
+	@: ## Do not remove this line, otherwise `make all` will trigger the `%` rule too.
+
+%:
+	BOARD_HARDWARE_PATH="$(BOARD_HARDWARE_PATH)" $(KALEIDOSCOPE_BUILDER_DIR)/kaleidoscope-builder $@
+
